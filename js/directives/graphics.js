4779f7912a4ced2ab97ad2c9cd44cfe764f7eb6c
Jeanette Santori <jeanette.santori@kitcheck.com>
2018-04-02 21:47:59 -0400

    still no resume

--- js/directives/graphics.js
+++ js/directives/graphics.js
@@ -1,4 +1,4 @@
-angular.module('todomvc')
+angular.module('jeanetteApp')
     .directive('jGraphics', function () {
         'use strict';
 
---
+++ js/directives/resume.js
@@ -0,0 +1,10 @@
+angular.module('jeanetteApp')
+    .directive('jResume', function () {
+        'use strict';
+
+        return {
+            restrict: 'E',
+            templateUrl: '/partials/resume.html'
+        };
+
+    });
